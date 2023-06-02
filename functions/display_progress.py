b7aaea2600d49be153c21b5679d5d8af4c572ee6
Harshith Kumar <123058145+Spyder67@users.noreply.github.com>
2023-06-02 08:06:49 +0530

    Update display_progress.py

--- functions/display_progress.py
+++ functions/display_progress.py
@@ -21,8 +21,8 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
         estimated_total_time = TimeFormatter(milliseconds=estimated_total_time)
 
         progress = "[{0}{1}]\n".format(
-            ''.join(["✦" for i in range(math.floor(percentage / 10))]),
-            ''.join(["✧" for i in range(10 - math.floor(percentage / 10))])
+            ''.join(["⬢" for i in range(math.floor(percentage / 10))]),
+            ''.join(["⬡" for i in range(10 - math.floor(percentage / 10))])
         )
 
         tmp = progress + Translation.PROGRESS.format(
